f343b979c784eface94dbe9f56795982c5742152
Viren Patil <viren.p2000@gmail.com>
2025-08-02 12:50:33 -0700

    user name visible

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Allow React frontend to talk to backend
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -12,6 +13,8 @@ app.add_middleware(
 )
 
 rooms = {}  # room_id: set of WebSocket connections
+usernames = {}  # websocket: username
+
 
 @app.websocket("/ws/{room_id}")
 async def websocket_endpoint(websocket: WebSocket, room_id: str):
@@ -30,15 +33,46 @@ async def websocket_endpoint(websocket: WebSocket, room_id: str):
     try:
         while True:
             data = await websocket.receive_text()
-            for conn in rooms[room_id]:
-                if conn != websocket:
-                    await conn.send_text(data)
+
+            # Lazy import to avoid JSON error on text check
+            import json
+            try:
+                payload = json.loads(data)
+            except json.JSONDecodeError:
+                continue
+
+            if payload["type"] == "name":
+                usernames[websocket] = payload["data"]
+
+                # Inform the other peer of this user's name
+                for conn in rooms[room_id]:
+                    if conn != websocket:
+                        await conn.send_text(json.dumps({
+                            "type": "name",
+                            "data": payload["data"]
+                        }))
+
+                # Also inform this websocket of the other user's name (if any)
+                for conn in rooms[room_id]:
+                    if conn != websocket and conn in usernames:
+                        await websocket.send_text(json.dumps({
+                            "type": "name",
+                            "data": usernames[conn]
+                        }))
+
+            else:
+                # Broadcast to other peer
+                for conn in rooms[room_id]:
+                    if conn != websocket:
+                        await conn.send_text(data)
+
     except WebSocketDisconnect:
         rooms[room_id].remove(websocket)
-
-        # Notify the other participant that their peer left
-        for conn in rooms.get(room_id, []):
-            await conn.send_text('{"type": "peer_left"}')
+        if websocket in usernames:
+            del usernames[websocket]
 
         if not rooms[room_id]:
             del rooms[room_id]
+        else:
+            for conn in rooms[room_id]:
+                await conn.send_text('{"type":"peer_left"}')
